213c4c060210511aebe71c19b669aceb48074595
gunnertwin <daniel@alvestechnology.io>
2022-09-28 12:25:54 +0100

    add terraform apply stage

--- provider.tf
+++ provider.tf
@@ -1,7 +1,12 @@
 terraform {
 
   required_version = ">=1.2.0"
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "RG-MDV-SHARED-01"
+    storage_account_name = "samdvshared01"
+    container_name       = "automation-terraform-functionapp"
+    key                  = "nonlive.terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
