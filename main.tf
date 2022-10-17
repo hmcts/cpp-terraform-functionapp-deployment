6a4f0f5574521516b45fcb6ca8c6fa599e316c45
varunvijayagopal <varun.vijayagopal@hmcts.net>
2022-10-17 14:08:37 +0100

    ei-862

--- main.tf
+++ main.tf
@@ -21,14 +21,14 @@ module "functionapp" {
   logicapp_parameters                       = var.logicapp_parameters
   logicapp_api_connection_template          = var.logicapp_api_connection_template
   logicapp_api_connection_parameters        = var.logicapp_api_connection_parameters
-  service_plan_name = var.service_plan_name ? var.service_plan_name : "as-${var.environment}-${var.namespace}-${var.application}"
-  create_storage_account = var.create_storage_account
-  create_service_plan = var.create_service_plan
-  namespace   = var.namespace
-  costcode    = var.costcode
-  attribute   = var.attribute
-  owner       = var.owner
-  environment = var.environment
-  application = var.application
-  type        = var.type
+  service_plan_name                         = var.service_plan_name ? var.service_plan_name : "as-${var.environment}-${var.namespace}-${var.application}"
+  create_storage_account                    = var.create_storage_account
+  create_service_plan                       = var.create_service_plan
+  namespace                                 = var.namespace
+  costcode                                  = var.costcode
+  attribute                                 = var.attribute
+  owner                                     = var.owner
+  environment                               = var.environment
+  application                               = var.application
+  type                                      = var.type
 }
