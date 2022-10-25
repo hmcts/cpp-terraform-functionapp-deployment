cdcd34f79eb2ef60e05e99c5f6f4e07ad246955e
varunvijayagopal <varun.vijayagopal@hmcts.net>
2022-10-25 08:12:41 +0100

    ei-862

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "functionapp" {
-  source                                    = "github.com/hmcts/cpp-module-terraform-azurerm-functionapp.git?ref=dev_ei_890"
+  source                                    = "github.com/hmcts/cpp-module-terraform-azurerm-functionapp.git?ref=main"
   storage_account_name                      = var.storage_account_name
   region                                    = var.region
   storage_account_tier                      = var.storage_account_tier
